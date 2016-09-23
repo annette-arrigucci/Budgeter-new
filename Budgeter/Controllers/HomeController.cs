affcf3c0ebe5e44f66f7a956bf94abbeb004332f
annette-arrigucci <annette.arrigucci@gmail.com>
2016-09-23 09:51:47 -0400

    added more code for getting budget vs. spending graph on Dashboard

--- Budgeter/Controllers/HomeController.cs
+++ Budgeter/Controllers/HomeController.cs
@@ -23,24 +23,40 @@ namespace Budgeter.Controllers
             DateTime currentDate = DateTime.Now;
             if (User.Identity.IsInHousehold())
             {
+                //get the household for this user
                 var householdId = User.Identity.GetHouseholdId();
                 var household = db.Households.Find(householdId);
+                //find the current budget for this user
                 if (household.Budgets.Where(x => x.Month == currentDate.Month).Where(x => x.Year == currentDate.Year).Any())
                 {
                     var currentBudget = household.Budgets.Where(x => x.Month == currentDate.Month).Where(x => x.Year == currentDate.Year).First();
-                    var budgetCategories = currentBudget.BudgetItems.GroupBy(x => x.CategoryId)
-                                           .Select(grp => grp.First()).ToList();
+                    //get all the expense categories for the current budget
+                    var budgetCategories = currentBudget.BudgetItems.Where(x => x.Type == "Expense").GroupBy(x => x.CategoryId)
+                                           .Select(grp => grp.First()).Select(x => x.CategoryId).ToList();
+                    //get the accounts for this household
                     var householdAccounts = household.Accounts.ToList();
+                    //get the transactions for this month
                     var monthTransactions = new List<Transaction>();
                     foreach(var a in householdAccounts)
                     {
-                        var transList = a.Transactions.Where(x => x.DateSpent.Year == currentDate.Year).Where(x => x.DateSpent.Month == currentDate.Month));
+                        var transList = a.Transactions.Where(x => x.DateSpent.Year == currentDate.Year).Where(x => x.DateSpent.Month == currentDate.Month);
                         monthTransactions.AddRange(transList);
                     }
-                    var transactionCategories = monthTransactions.GroupBy(x => x.CategoryId)
-                                                .Select(grp => grp.First()).ToList();
+                    //get the expense transaction categories
+                    var transactionCategories = monthTransactions.Where(x => x.Type == "Expense").GroupBy(x => x.CategoryId)
+                                                .Select(grp => grp.First()).Select(x => x.CategoryId).ToList();
                     //now we have a list of categories for budget and transactions, merge them into one master list
-
+                    var masterCategoriesList = budgetCategories;
+                    masterCategoriesList.AddRange(transactionCategories);
+                    masterCategoriesList = masterCategoriesList.Distinct().ToList();
+                    ViewBag.CategoriesList = masterCategoriesList;
+                    foreach(var c in masterCategoriesList)
+                    {
+                        var category = db.Categories.Find(c);
+                        var categoryName = category.Name;
+                        var budgetCategoryTotal = currentBudget.BudgetItems.Where(x => x.CategoryId == c);
+                        var transactionCategoryTotal = monthTransactions.Where(x => x.CategoryId == c);
+                    }
 
                 }
                 else
@@ -49,7 +65,8 @@ namespace Budgeter.Controllers
                 }
 
             }
-            ViewBag.Message = "This is the dashboard page.";
+            
+            //ViewBag.Message = "This is the dashboard page.";
 
             return View();
         }
---
+++ Budgeter/Models/SpendingCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Budgeter.Models
+{
+    public class SpendingCategory
+    {
+        public string CategoryName { get; set; }
+        public Decimal BudgetTotal { get; set; }
+        public Decimal TransactionTotal { get; set; }
+    }
+}
