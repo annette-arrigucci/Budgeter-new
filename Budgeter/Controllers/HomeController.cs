0d727875351b5145df3d6d017c431df94556a1c9
annette-arrigucci <annette.arrigucci@gmail.com>
2016-09-23 09:10:12 -0400

    working on code for Dashboard

--- Budgeter/Controllers/HomeController.cs
+++ Budgeter/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace Budgeter.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return RedirectToAction("Dashboard", "Home");
@@ -18,6 +20,35 @@ namespace Budgeter.Controllers
         [Authorize]
         public ActionResult Dashboard()
         {
+            DateTime currentDate = DateTime.Now;
+            if (User.Identity.IsInHousehold())
+            {
+                var householdId = User.Identity.GetHouseholdId();
+                var household = db.Households.Find(householdId);
+                if (household.Budgets.Where(x => x.Month == currentDate.Month).Where(x => x.Year == currentDate.Year).Any())
+                {
+                    var currentBudget = household.Budgets.Where(x => x.Month == currentDate.Month).Where(x => x.Year == currentDate.Year).First();
+                    var budgetCategories = currentBudget.BudgetItems.GroupBy(x => x.CategoryId)
+                                           .Select(grp => grp.First()).ToList();
+                    var householdAccounts = household.Accounts.ToList();
+                    var monthTransactions = new List<Transaction>();
+                    foreach(var a in householdAccounts)
+                    {
+                        var transList = a.Transactions.Where(x => x.DateSpent.Year == currentDate.Year).Where(x => x.DateSpent.Month == currentDate.Month));
+                        monthTransactions.AddRange(transList);
+                    }
+                    var transactionCategories = monthTransactions.GroupBy(x => x.CategoryId)
+                                                .Select(grp => grp.First()).ToList();
+                    //now we have a list of categories for budget and transactions, merge them into one master list
+
+
+                }
+                else
+                {
+                    ViewBag.Message = "No budget exists for this month";
+                }
+
+            }
             ViewBag.Message = "This is the dashboard page.";
 
             return View();
